99ed0f17153ab42ec084647056eeee1ecc59d819
Andy Baxter <andrewbaxter439@gmail.com>
2025-09-17 16:29:28 +0100

    safe fetch LB receipt (sometimes null)

--- src/main/java/simpaths/model/Person.java
+++ src/main/java/simpaths/model/Person.java
@@ -5101,6 +5101,14 @@ public class Person implements EventListener, IDoubleSource, IIntSource, Weight,
         return receivesBenefitsFlagUC;
     }
 
+    public Double isReceivesBenefitsUCDouble() {
+        return receivesBenefitsFlagUC ? 1.0 : 0.0;
+    }
+
+    public Double isReceivesBenefitsLBDouble() {
+        return (null != receivesBenefitsFlagLB && receivesBenefitsFlagLB) ? 1.0 : 0.0;
+    }
+
     public void setReceivesBenefitsFlagUC(boolean receivesBenefitsFlagUC) {
         this.receivesBenefitsFlagUC = receivesBenefitsFlagUC;
     }
