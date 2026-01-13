694d3d11963511e9220a3d9d70f4def407c3784e
Andy Baxter <andrewbaxter439@gmail.com>
2026-01-13 10:57:31 +0000

    added constrain estimates when not updating stage 2

--- src/main/java/simpaths/model/Person.java
+++ src/main/java/simpaths/model/Person.java
@@ -1063,6 +1063,8 @@ public class Person implements EventListener, IDoubleSource, IIntSource, Weight,
                 dhmPrediction = Parameters.getRegHealthHM2LevelFemales().getScore(this, Person.DoublesVariables.class);
                 dhm = constrainDhmEstimate(dhmPrediction+dhm);
             } else System.out.println("healthMentalHM2 method in Person class: Person has no gender!");
+        } else if (dhm != null) {
+            dhm = constrainDhmEstimate(dhm);
         }
     }
 
@@ -1106,6 +1108,8 @@ public class Person implements EventListener, IDoubleSource, IIntSource, Weight,
                 dhmGhqPrediction = Parameters.getRegHealthHM2CaseFemales().getScore(this, Person.DoublesVariables.class);
                 dhmGhq = constrainDhmGhqEstimate(dhmGhqPrediction+dhmGhq);
             } else System.out.println("healthMentalHM2 method in Person class: Person has no gender!");
+        } else if (dhmGhq != null) {
+            dhmGhq = constrainDhmGhqEstimate(dhmGhq);
         }
     }
 
@@ -1151,6 +1155,8 @@ public class Person implements EventListener, IDoubleSource, IIntSource, Weight,
                 mcsPrediction = Parameters.getRegHealthMCS2Females().getScore(this, Person.DoublesVariables.class);
                 dhe_mcs = constrainSF12Estimate(mcsPrediction + dhe_mcs);
             }
+        } else if (dhe_mcs != null) {
+            dhe_mcs = constrainSF12Estimate(dhe_mcs);
         }
     }
 
@@ -1196,6 +1202,8 @@ public class Person implements EventListener, IDoubleSource, IIntSource, Weight,
                 pcsPrediction = Parameters.getRegHealthPCS2Females().getScore(this, Person.DoublesVariables.class);
                 dhe_pcs = constrainSF12Estimate(pcsPrediction + dhe_pcs);
             }
+        } else if (dhe_pcs != null) {
+            dhe_pcs = constrainSF12Estimate(dhe_pcs);
         }
     }
 
@@ -1244,6 +1252,8 @@ public class Person implements EventListener, IDoubleSource, IIntSource, Weight,
                 dlsPrediction = Parameters.getRegLifeSatisfaction2Females().getScore(this, Person.DoublesVariables.class);
                 dls = constrainLifeSatisfactionEstimate(dlsPrediction + dls);
             }
+        } else if (dls != null) {
+            dls = constrainLifeSatisfactionEstimate(dls);
         }
     }
 
