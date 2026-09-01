8d54fb917e63e5ab5935f78ab7f9e7d664b5bbde
Andy Baxter <andrewbaxter439@gmail.com>
2026-09-01 22:29:28 +0100

    revert to non-UC method of determining labour supply

--- src/main/java/simpaths/model/LabourMarket.java
+++ src/main/java/simpaths/model/LabourMarket.java
@@ -259,7 +259,7 @@ public class LabourMarket {
 
             //Update Labour Supply
             benefitUnitsAllRegions.parallelStream()
-                    .forEach(BenefitUnit::updateLabourSupplyAndIncomeWithUniversalCredit);
+                    .forEach(BenefitUnit::updateLabourSupplyAndIncome);
 
             Map<Education, Double> potentialHourlyEarningsByEdu = new LinkedHashMap<Education, Double>();
             Map<Education, Integer> countByEdu = new LinkedHashMap<Education, Integer>();
